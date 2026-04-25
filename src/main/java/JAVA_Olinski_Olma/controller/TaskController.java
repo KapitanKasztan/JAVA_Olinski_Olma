78a886fa3237a6b8b9e2244dbeb0df94e5daf225
Jakub Olma <olmak16@gmail.com>
2026-04-25 15:57:13 +0200

    Lab 3 zad 7: Dodano opisy do Put i Delete oraz zaaktualizowano opisy innych

--- src/main/java/JAVA_Olinski_Olma/controller/TaskController.java
+++ src/main/java/JAVA_Olinski_Olma/controller/TaskController.java
@@ -3,6 +3,7 @@ package JAVA_Olinski_Olma.controller;
 import JAVA_Olinski_Olma.model.Task;
 import JAVA_Olinski_Olma.services.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
@@ -21,29 +22,34 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @Operation(summary = "Retrieve all tasks")
+    @Operation(summary = "Retrieve all tasks", description = "Fetches a list of all tasks from the database")
     @GetMapping("/all")
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
     }
 
-    @Operation(summary = "Create a new task")
+    @Operation(summary = "Create a new task", description = "Creates a new task with the provided details")
     @PostMapping("/create")
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
         Task createdTask = taskService.createTask(task);
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 
-    @Operation(summary = "Update an existing task")
+    @Operation(summary = "Update an existing task", description = "Updates the task with the specified ID")
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+    public ResponseEntity<Task> updateTask(
+            @Parameter(name = "id", description = "The unique identifier of the task to update", required = true, example = "1")
+            @PathVariable Long id,
+            @RequestBody Task taskDetails) {
         var updatedTask = taskService.updateTask(id, taskDetails);
         return updatedTask.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @Operation(summary = "Delete a task by ID")
+    @Operation(summary = "Delete a task by ID", description = "Deletes the task with the specified ID from the database")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteTask(
+            @Parameter(name = "id", description = "The unique identifier of the task to delete", required = true, example = "1")
+            @PathVariable Long id) {
         if (taskService.deleteTask(id)) {
             return ResponseEntity.noContent().build();
         }
