194842655167ed5ac1618d2af743807d84400767
adamolinskipk <adam.olinski@proton.me>
2026-04-25 14:47:07 +0200

    Aktualizacja plików UserController.java, TaskController.java i ProjectController.java

--- src/main/java/JAVA_Olinski_Olma/controller/TaskController.java
+++ src/main/java/JAVA_Olinski_Olma/controller/TaskController.java
@@ -2,26 +2,32 @@ package JAVA_Olinski_Olma.controller;
 
 import JAVA_Olinski_Olma.model.Task;
 import JAVA_Olinski_Olma.repository.TaskRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Tasks", description = "Operations on tasks")
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
-private final TaskRepository taskRepository;
 
-public TaskController(TaskRepository taskRepository) {
-    this.taskRepository = taskRepository;
-}
+    private final TaskRepository taskRepository;
 
-@GetMapping
-public List<Task> getAllTasks() {
-    return taskRepository.findAll();
-}
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
-@PostMapping
-public Task createTask(@RequestBody Task task) {
-    return taskRepository.save(task);
-}
+    @Operation(summary = "Retrieve all tasks")
+    @GetMapping("/all")
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @Operation(summary = "Create a new task")
+    @PostMapping("/create")
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
 }
--- src/main/java/JAVA_Olinski_Olma/controller/UserController.java
+++ src/main/java/JAVA_Olinski_Olma/controller/UserController.java
@@ -2,26 +2,31 @@ package JAVA_Olinski_Olma.controller;
 
 import JAVA_Olinski_Olma.model.Users;
 import JAVA_Olinski_Olma.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Users", description = "Operacje na u?ytkownikach")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-public UserController(UserRepository userRepository) {
-    this.userRepository = userRepository;
-}
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-@GetMapping
-public List<Users> getAllUsers() {
-    return userRepository.findAll();
-}
+    @Operation(summary = "Retrieve all users")
+    @GetMapping("/all")
+    public List<Users> getAllUsers() {
+        return userRepository.findAll();
+    }
 
-@PostMapping
-public Users createUser(@RequestBody Users user) {
-    return userRepository.save(user);
-}
+    @Operation(summary = "Create a new user")
+    @PostMapping("/create")
+    public Users createUser(@RequestBody Users user) {
+        return userRepository.save(user);
+    }
 }
